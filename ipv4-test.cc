7643b5ed6a5ca16cae4bbf674b8eb2ac666f2391
joseaguerrar <joseaguerrar2017@gmail.com>
2025-03-15 14:51:24 -0600

    prueba con otros comentarios y la IP pública

--- ipv4-test.cc
+++ ipv4-test.cc
@@ -11,15 +11,15 @@
  
  int main( int argc, char * argv[] ) {
     const char * os = "http://os.ecci.ucr.ac.cr/";
-    const char * osi = "10.84.166.62";
-    const char * ose = "163.178.104.62";
+    const char * osi = "10.84.166.62"; // IP privada
+    const char * ose = "163.178.104.62";  // IP publica
     const char * whale = (char *) "GET /aArt/index.php?disk=Disk-01&fig=whale-1.txt\r\nHTTP/v1.1\r\nhost: redes.ecci\r\n\r\n";
  
     VSocket * s;	
     char a[512];
  
     s = new Socket( 's' );
-    s->MakeConnection( osi, 80 );
+    s->MakeConnection( ose, 80 );
     s->Write( whale );
     s->Read( a, 512 );
     printf( "%s\n", a);
