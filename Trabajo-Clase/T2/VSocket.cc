2fb7e8d3acb9bbf3cb88f9eb3bb811cafb217256
joseaguerrar <joseaguerrar2017@gmail.com>
2025-03-20 17:02:10 -0600

    Cambio en los métodos de establecer conexiónes en protocólo UDP, faltan: BIND, SENDTO, RECVFROM

--- Trabajo-Clase/T2/VSocket.cc
+++ Trabajo-Clase/T2/VSocket.cc
@@ -38,12 +38,16 @@
  **/
 void VSocket::BuildSocket( char t, bool IPv6 ){
 
-   int st = -1;
+   this->IPv6 = IPv6;
+   this->type = t;
+   
+   int domain = (IPv6) ? AF_INET6 : AF_INET;
+   int socketType = (t == 's') ? SOCK_STREAM : SOCK_DGRAM;
 
-   if ( -1 == st ) {
-      throw std::runtime_error( "VSocket::BuildSocket, (reason)" );
+   this->idSocket = socket( domain, socketType, 0 );
+   if ( -1 == this->idSocket ) {
+      throw std::runtime_error( "VSocket::BuildSocket(), error al crear socket" );
    }
-
 }
 
 
@@ -64,10 +68,8 @@ VSocket::~VSocket() {
   *
  **/
 void VSocket::Close(){
-   int st = -1;
-
-   if ( -1 == st ) {
-      throw std::runtime_error( "VSocket::Close()" );
+   if (close(this->idSocket) == -1) {
+      throw std::runtime_error( "VSocket::Close(), error al cerrar socket" );
    }
 
 }
@@ -82,14 +84,24 @@ void VSocket::Close(){
   *
  **/
 int VSocket::EstablishConnection( const char * hostip, int port ) {
+   if (this->type != 's') { // Checkear que no sea UDP
+      throw std::runtime_error("VSocket::EstablishConnection(), no válido para UDP");
+   }
+   struct sockaddr_in server_addr; // Estructura para dirección del servidor
+   memset( &server_addr, 0, sizeof(server_addr) ); // Limpiar estructura
+   server_addr.sin_family = AF_INET; // IPv4
+   server_addr.sin_port = htons( port ); 
 
-   int st = -1;
-
-   if ( -1 == st ) {
-      throw std::runtime_error( "VSocket::EstablishConnection" );
+   if ( inet_pton( AF_INET, hostip, &server_addr.sin_addr ) <= 0 ) {
+      perror( "inet_pton error" ); 
+      throw std::runtime_error( "VSocket::EstablishConnection(), error en inet_pton" ); // Error inet_pton
    }
 
-   return st;
+   if ( connect( this->idSocket, (struct sockaddr *) &server_addr, sizeof(server_addr) ) == -1 ) {
+      perror( "connect error" );
+      throw std::runtime_error( "VSocket::EstablishConnection(), error al conectar" );  // Error connect
+   }
+   return 0;
 
 }
 
@@ -103,11 +115,34 @@ int VSocket::EstablishConnection( const char * hostip, int port ) {
   *
  **/
 int VSocket::EstablishConnection( const char *host, const char *service ) {
-   int st = -1;
+   if (this->type != 's') { // Checkear que no sea UDP
+      throw std::runtime_error("VSocket::EstablishConnection(), no válido para UDP");
+   }
+   struct addrinfo hints, *res, *rp;
+    
+    // Limpiar estructura hints y definir criterios de búsqueda
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = this->IPv6 ? AF_INET6 : AF_INET;  // IPv4 o IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP
 
-   throw std::runtime_error( "VSocket::EstablishConnection" );
+    // Obtener información de la dirección usando getaddrinfo
+    int status = getaddrinfo(host, service, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return -1; // Retornar -1 si hubo un error en la resolución del host
+    }
 
-   return st;
+    // Iterar sobre todas las direcciones devueltas y tratar de conectar
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        if (connect(this->idSocket, rp->ai_addr, rp->ai_addrlen) == 0) {
+            freeaddrinfo(res);
+            return 0;  // Conexión exitosa
+        }
+    }
+
+    // Si llegamos aquí, ninguna dirección funcionó
+    freeaddrinfo(res);
+    return -1; // Indicar error si no se pudo conectar a ninguna dirección
 
 }
 
