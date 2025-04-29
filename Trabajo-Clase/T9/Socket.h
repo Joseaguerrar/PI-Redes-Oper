fd45666ee18d08dbbb3ed13c5fb2d0973a631413
joseaguerrar <joseaguerrar2017@gmail.com>
2025-04-29 09:24:53 -0600

    Archivos faltantes de sockets, falta implementación de sslsocket

---
+++ Trabajo-Clase/T9/Socket.h
@@ -0,0 +1,35 @@
+/**
+  *  Universidad de Costa Rica
+  *  ECCI
+  *  CI0123 Proyecto integrador de redes y sistemas operativos
+  *  2025-i
+  *  Grupos: 1 y 3
+  *
+  ****** Socket class interface
+  *
+  * (Fedora version)
+  *
+ **/
+
+ #ifndef Socket_h
+ #define Socket_h
+
+ #include <cstddef>
+ 
+ class Socket {
+ 
+    public:
+       Socket( char, bool = false );
+       Socket( int ); // Constructor adicional para aceptar un descriptor
+       ~Socket();
+       int MakeConnection( const char *, int );
+       int MakeConnection( const char *, const char * );
+       size_t Read( void *, size_t );
+       size_t Write( const void *, size_t );
+       size_t Write( const char * );
+ 
+    protected:
+ 
+ };
+ 
+ #endif
